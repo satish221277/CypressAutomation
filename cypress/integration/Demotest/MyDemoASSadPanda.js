ebd46239c0db18413a9b080ccdc66c701a6a2b8f
Satish <satish.kumar@rategain.com>
2021-06-23 21:07:35 +0530

    Changes in sad panda

--- cypress/integration/Demotest/MyDemoASSadPanda.js
+++ cypress/integration/Demotest/MyDemoASSadPanda.js
@@ -43,7 +43,7 @@ cy.get('.clientview-sidebar--inner > ul > :nth-child(4) > a > img').click()
 cy.get('.Reports__CardsOrPresetsDropdown__Wrapper > [data-testid=common-select-dropdown] > [data-testid=SelectToggleDropdownOpen] > .display').click()
 cy.get(':nth-child(5) > .Reports__PresetButton__Addon > .fa').click()
 cy.wait(10000)
-cy.get('.Reports__ReportsGroup__Container > :nth-child(1)').then(function(element)
+cy.get('.Reports__ReportsGroup__Container > :nth-child(2)').then(function(element)
 
 {
 const actualtext=element.text()
